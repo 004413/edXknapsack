32f830e48e5bd0b5cee3c5b2d18742dcca4529ee
Ray Hua Wu <d684n@mit.edu>
2014-06-17 17:13:33 -0400

    knapsack update

--- knapsack.js
+++ knapsack.js
@@ -1,31 +1,41 @@
 $(document).ready(function(){ // movement function
   $('.alert').hide();
+  var value = 0;
+  var mass = 0;
   var house = $('#house');
   var sack = $('#sack');
-  var houseItems = $('#house .item');
-  var sackItems = $('#sack .item');
-  houseItems.on('click', function(event){ // when clicked while in house, move to sack
-    var target = $(this);
-    sack.append(target);
-    houseItems.remove(target);
-    houseItems = $('#house .item');
-    sackItems = $('#sack .item');
-    house = $('#house');
-    sack = $('#sack');
+  var items = $('.item');
+  items.click(function(){
+    if($(this).parent()
+              .attr('id')=='house'){
+      sack.append( $(this) );
+      mass += parseInt($(this).children('img')
+                              .attr('data-weight'));
+      value += parseInt($(this).children('img')
+                               .attr('data-value'));
+    }else{
+      house.append( $(this) );
+      mass -= parseInt($(this).children('img')
+                              .attr('data-weight'));
+      value -= parseInt($(this).children('img')
+                               .attr('data-value'));
+    }
   });
-  sackItems.on('click', function(event){ // when clicked while in sack, move to house
+/*  sackItems.on('click', function(event){ // when clicked while in sack, move to house
+    console.log("Also hi here.");
     var target = $(this);
     console.log("Hi there.");
     house.append(target);
-    sackItems.remove(target);
+    sackItems.detach(target);
     sackItems = $('#sack .item');
     houseItems = $('#house .item');
     sack = $('#sack');
     house = $('#house');
-  });
+  });*/
 });
 
 function overmass(){
-  $('.alert').show(200).delay(200).hide(200);
-
+  $('.alert').show(200)
+             .delay(200)
+             .hide(200);
 }
