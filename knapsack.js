6f0f7829034a672fcd1dec9640010ef5840ea02f
Ray Hua Wu <d684n@mit.edu>
2014-06-17 14:12:02 -0400

    updated knapsack.html and knapsack.js

--- knapsack.js
+++ knapsack.js
@@ -1,16 +1,31 @@
 $(document).ready(function(){ // movement function
+  $('.alert').hide();
   var house = $('#house');
   var sack = $('#sack');
   var houseItems = $('#house .item');
   var sackItems = $('#sack .item');
   houseItems.on('click', function(event){ // when clicked while in house, move to sack
-    var target = $(event.target);
+    var target = $(this);
     sack.append(target);
-    house.remove(target);
+    houseItems.remove(target);
+    houseItems = $('#house .item');
+    sackItems = $('#sack .item');
+    house = $('#house');
+    sack = $('#sack');
   });
   sackItems.on('click', function(event){ // when clicked while in sack, move to house
-    var target = $(event.target);
+    var target = $(this);
+    console.log("Hi there.");
     house.append(target);
-    sack.remove(target);
+    sackItems.remove(target);
+    sackItems = $('#sack .item');
+    houseItems = $('#house .item');
+    sack = $('#sack');
+    house = $('#house');
   });
-})
+});
+
+function overmass(){
+  $('.alert').show(200).delay(200).hide(200);
+
+}
